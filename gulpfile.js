a3228e3ed42adc8a6b4d082ac8e5ecee0eac7e53
awet melake <awetmelake@gmail.com>
2019-09-24 14:32:17 -0500

    upgraded syntax to v4 standards

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,50 @@
-const gulp = require("gulp");
+const { task, src, watch, series, parallel, dest } = require("gulp");
 const imagemin = require("gulp-imagemin");
 const uglify = require("gulp-uglify");
 const sass = require("gulp-sass");
 
 // copy html
 function html(done) {
- gulp.src("src/*.html").pipe(gulp.dest("dist"));
- done();
+  src("src/*.html").pipe(dest("dist"));
+  done();
 }
 
 // optimize images
 function image(done) {
-  gulp
-    .src("src/images/*")
+  src("src/images/*")
     .pipe(imagemin())
-    .pipe(gulp.dest("dist/images"));
+    .pipe(dest("dist/images"));
   done();
 }
 
 // minify js
 function js(done) {
-  gulp
-    .src("src/js/*.js")
+  src("src/js/*.js")
     .pipe(uglify())
-    .pipe(gulp.dest("dist"));
-    done();
+    .pipe(dest("dist"));
+  done();
 }
 
 // compile sass
 function css(done) {
-  gulp
-    .src("src/sass/*.sass")
+  src("src/sass/*.sass")
     .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest("dist"));
-    done();
+    .pipe(dest("dist"));
+  done();
 }
 
 // watch files for changes
-function watch_files(){
-    gulp.watch("src/images/*", image);
-    gulp.watch("src/sass/*.sass", css);
-    gulp.watch("src/js/*.js", js);
-    gulp.watch("src/*.html", html);
+function watch_files() {
+  watch("src/images/*", image);
+  watch("src/sass/*.sass", css);
+  watch("src/js/*.js", js);
+  watch("src/*.html", html);
 }
 
 // tasks
-gulp.task("css", css);
-gulp.task("js", js);
-gulp.task("image", image);
-gulp.task("html", html);
-gulp.task("default", gulp.parallel(css, js, image, html));
-gulp.task("watch", gulp.series(watch_files));
+task("css", css);
+task("js", js);
+task("image", image);
+task("html", html);
+task("default", parallel(css, js, image, html));
+task("watch", series(watch_files));
