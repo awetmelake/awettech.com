2e8c563f32321fdab7cc80812de0383f2f2f868e
awet melake <awetmelake@gmail.com>
2019-09-24 14:27:32 -0500

    upgraded syntax to v4 standards

--- gulpfile.js
+++ gulpfile.js
@@ -3,30 +3,51 @@ const imagemin = require("gulp-imagemin");
 const uglify = require("gulp-uglify");
 const sass = require("gulp-sass");
 
-gulp.task("defualt", function() {});
-gulp.task("copyHtml", function() {
-  gulp.src("src/*.html").pipe(gulp.dest("dist"));
-});
+// copy html
+function html(done) {
+ gulp.src("src/*.html").pipe(gulp.dest("dist"));
+ done();
+}
 
-gulp.task("imageMin", function() {
+// optimize images
+function image(done) {
   gulp
     .src("src/images/*")
     .pipe(imagemin())
     .pipe(gulp.dest("dist/images"));
-});
+  done();
+}
 
 // minify js
-gulp.task("minify", function() {
+function js(done) {
   gulp
     .src("src/js/*.js")
     .pipe(uglify())
     .pipe(gulp.dest("dist"));
-});
+    done();
+}
 
 // compile sass
-gulp.task("sass", function() {
+function css(done) {
   gulp
     .src("src/sass/*.sass")
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass().on("error", sass.logError))
     .pipe(gulp.dest("dist"));
-});
+    done();
+}
+
+// watch files for changes
+function watch_files(){
+    gulp.watch("src/images/*", image);
+    gulp.watch("src/sass/*.sass", css);
+    gulp.watch("src/js/*.js", js);
+    gulp.watch("src/*.html", html);
+}
+
+// tasks
+gulp.task("css", css);
+gulp.task("js", js);
+gulp.task("image", image);
+gulp.task("html", html);
+gulp.task("default", gulp.parallel(css, js, image, html));
+gulp.task("watch", gulp.series(watch_files));
